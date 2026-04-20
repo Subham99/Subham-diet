5b17b5c62f1b7d4b5e96ae11dbb1f65ecf8a2a10
Subham99 <40452262+Subham99@users.noreply.github.com>
2026-04-20 21:38:21 +0530

    Create src/index.jsx file for Subham diet plan application

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Subham Diet Plan</h1>
+      <p>Your diet plan content goes here!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
